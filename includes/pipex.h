c3529ca581b87b8a07ae0d9e0054912432f63df3
Natalie Harutyunyan <natalieyan@Natalies-Laptop.local>
2025-05-07 15:57:41 +0400

    ihopeso

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: natalieyan <natalieyan@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 02:43:47 by natalieyan        #+#    #+#             */
-/*   Updated: 2025/05/05 20:11:03 by natalieyan       ###   ########.fr       */
+/*   Updated: 2025/05/07 15:56:18 by natalieyan       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,31 @@
 # include <sys/wait.h>
 # include <unistd.h>
 
-void	free_split(char **arr);
-char	*get_path(char *cmd, char **envp);
-char	**parse_cmd(char *cmd);
-int		open_infile(char *filename);
-int		open_outfile(char *filename);
-void	error_exit(char *msg);
+typedef struct s_exec_info
+{
+	char		**argv;
+	char		**envp;
+	int			*pipefd;
+	int			input_fd;
+	int			output_fd;
+}				t_exec_info;
+
+typedef struct s_main_data
+{
+	int			pipefd[2];
+	pid_t		pid1;
+	pid_t		pid2;
+	int			infile;
+	int			outfile;
+	t_exec_info	child_info;
+	t_exec_info	parent_info;
+}				t_main_data;
+
+void			free_split(char **arr);
+char			*get_path(char *cmd, char **envp);
+char			**parse_cmd(char *cmd);
+int				open_infile(char *filename);
+int				open_outfile(char *filename);
+void			error_exit(char *msg);
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: natalieyan <natalieyan@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 02:44:23 by natalieyan        #+#    #+#             */
-/*   Updated: 2025/05/05 21:25:32 by natalieyan       ###   ########.fr       */
+/*   Updated: 2025/05/07 15:56:21 by natalieyan       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,50 +25,53 @@ char	**parse_cmd(char *cmd)
 	return (args);
 }
 
-void	child_process(char **argv, char **envp, int *pipefd, int infile)
+void	child_process(t_exec_info *info)
 {
 	char	**cmd_args;
 	char	*cmd_path;
 
-	if (dup2(infile, STDIN_FILENO) < 0 || dup2(pipefd[1], STDOUT_FILENO) < 0)
-		error_exit("dup2 (child)");
-	close(pipefd[0]);
-	cmd_args = parse_cmd(argv[2]);
-	cmd_path = get_path(cmd_args[0], envp);
+	if (dup2(info->input_fd, STDIN_FILENO) < 0)
+		error_exit("dup2 (child - input)");
+	if (dup2(info->pipefd[1], STDOUT_FILENO) < 0)
+		error_exit("dup2 (child - pipe)");
+	close(info->pipefd[0]);
+	cmd_args = parse_cmd(info->argv[2]);
+	cmd_path = get_path(cmd_args[0], info->envp);
 	if (!cmd_path)
 	{
 		free_split(cmd_args);
 		error_exit("Command not found");
 	}
-	execve(cmd_path, cmd_args, envp);
+	execve(cmd_path, cmd_args, info->envp);
 	free_split(cmd_args);
 	free(cmd_path);
 	error_exit("execve (child)");
 }
 
-void	parent_process(char **argv, char **envp, int *pipefd, int outfile)
+void	parent_process(t_exec_info *info)
 {
 	char	**cmd_args;
 	char	*cmd_path;
 
-	if (dup2(pipefd[0], STDIN_FILENO) < 0 || dup2(outfile, STDOUT_FILENO) < 0)
-		error_exit("dup2 (parent)");
-	close(pipefd[1]);
-	cmd_args = parse_cmd(argv[3]);
-	cmd_path = get_path(cmd_args[0], envp);
+	if (dup2(info->pipefd[0], STDIN_FILENO) < 0)
+		error_exit("dup2 (parent - pipe)");
+	if (dup2(info->output_fd, STDOUT_FILENO) < 0)
+		error_exit("dup2 (parent - output)");
+	close(info->pipefd[1]);
+	cmd_args = parse_cmd(info->argv[3]);
+	cmd_path = get_path(cmd_args[0], info->envp);
 	if (!cmd_path)
 	{
 		free_split(cmd_args);
 		error_exit("Command not found");
 	}
-	execve(cmd_path, cmd_args, envp);
+	execve(cmd_path, cmd_args, info->envp);
 	free_split(cmd_args);
 	free(cmd_path);
 	error_exit("execve (parent)");
 }
 
-pid_t	fork_process(void (*func)(char **, char **, int *, int), char **argv,
-		char **envp, int *pipefd, int fd)
+pid_t	fork_process(void (*func)(t_exec_info *), t_exec_info *info)
 {
 	pid_t	pid;
 
@@ -76,31 +79,29 @@ pid_t	fork_process(void (*func)(char **, char **, int *, int), char **argv,
 	if (pid == -1)
 		error_exit("Fork error");
 	if (pid == 0)
-		func(argv, envp, pipefd, fd);
+		func(info);
 	return (pid);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	int		pipefd[2];
-	pid_t	pid1;
-	pid_t	pid2;
-	int		infile;
-	int		outfile;
+	t_main_data	data;
 
 	if (argc != 5)
 		error_exit("Usage: ./pipex file1 cmd1 cmd2 file2");
-	infile = open_infile(argv[1]);
-	outfile = open_outfile(argv[4]);
-	if (infile < 0 || outfile < 0)
+	data.infile = open_infile(argv[1]);
+	data.outfile = open_outfile(argv[4]);
+	if (data.infile < 0 || data.outfile < 0)
 		error_exit("File error");
-	if (pipe(pipefd) == -1)
+	if (pipe(data.pipefd) == -1)
 		error_exit("Pipe error");
-	pid1 = fork_process(child_process, argv, envp, pipefd, infile);
-	pid2 = fork_process(parent_process, argv, envp, pipefd, outfile);
-	close(pipefd[0]);
-	close(pipefd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	data.child_info = (t_exec_info){argv, envp, data.pipefd, data.infile, -1};
+	data.parent_info = (t_exec_info){argv, envp, data.pipefd, -1, data.outfile};
+	data.pid1 = fork_process(child_process, &data.child_info);
+	data.pid2 = fork_process(parent_process, &data.parent_info);
+	close(data.pipefd[0]);
+	close(data.pipefd[1]);
+	waitpid(data.pid1, NULL, 0);
+	waitpid(data.pid2, NULL, 0);
 	return (0);
 }
